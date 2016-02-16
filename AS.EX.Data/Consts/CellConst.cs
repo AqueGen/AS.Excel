8d8bbaeb5770fcf746d1b2115ecd0d498384a98f
Artem Frolov <aquegen@yandex.ru>
2016-02-16 18:09:22 +0200

    чистка

--- AS.EX.Data/Consts/CellConst.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AS.EX.Model.Consts
-{
-    public class CellConst
-    {
-        public const string ExpressionSymbol = "=";
-        public const string TextSymbol = "'";
-    }
-}
--- AS.EX.Data/Consts/TableCellConst.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AS.EX.Model.Consts
-{
-    public class TableCellConst
-    {
-        public const int StartRowIndex = 1;
-    }
-}
